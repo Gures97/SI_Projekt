20e6b9025f3716a7196a379fb84ee15f3dbf9390
Natalia <mcb.nat@gmail.com>
2018-06-22 15:20:36 +0200

    Zaimplementowano GA

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,18 @@
-import java.io.File;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
-
 public class Main {
-
 	public static void main(String[] args) {
-		Scanner in, inner;
-		
-		ClasificationSet<Iris> dane = new ClasificationSet<Iris>();
-
-        try{
-            in = new Scanner(new File("dataset.txt"));
-        }
-        catch(Exception ex){
-            System.out.println(ex.getMessage());
-            return;
-        }
-        double sl,sw,pl,pw;
-        String typ, line;
-        while(in.hasNext()) {
-        	line = in.nextLine();
-        	line = line.replace(',', ' ');
-        	line = line.replace('.', ',');
-        	inner = new Scanner(line);
-        	sl = inner.nextDouble();
-        	sw = inner.nextDouble();
-        	pl = inner.nextDouble();
-        	pw = inner.nextDouble();
-        	typ = inner.next();
-        	dane.addExample(new Iris(sl,sw,pl,pw,typ));
-        }
-        
-        in.close();
-
-        //Coś robienie z danymi
-
-        PrintWriter out;
-
-        try{
-            out = new PrintWriter(new File("Out.txt"));
-        }
-        catch(Exception ex){
-            System.out.println(ex.getMessage());
-            return;
-        }
-        
-        for(Iris i : dane.data) {
-        	out.println(i.sepalLength + ", " 
-        				+ i.sepalWidth + ", " 
-        				+ i.petalLength + ", " 
-        				+ i.petalWidth + ", "
-        				+ i.type);
-        }
-        out.close();
-        System.out.println("Już");
-
+		Population pop = new Population(1000);
+		for(int i = 0; true; i++) {
+			if (i%10 == 0) {
+				System.out.println(pop);
+			}
+			pop.live();
+			pop.reproduce();
+			if(pop.getPercentFitted() > 0.1) {
+				break;
+			}
+		} 
+		pop.live();
+		System.out.println(pop);
 	}
 
 }
